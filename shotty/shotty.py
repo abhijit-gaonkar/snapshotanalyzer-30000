a5a2460678dc25f9119a2eb8e87e87620e2e0b7e
Abhijit Gaonkar <gaonkara@dnb.com>
2019-11-27 16:20:28 -0800

    snapshots and exception handling

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -19,9 +20,53 @@ def filter_instances(project):
 def cli():
 	"""Shotty manages snapshots"""
 
+@cli.group('snapshots')
+def snapshots():
+	"""Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+	help="Only instances for project (tag Project:<name>)")
+def list_snapshots(project):
+	"List Snapshots"
+	instances = filter_instances(project)
+
+	for i in instances:
+		for v in i.volumes.all():
+			for s in v.snapshots.all():
+				print(", ".join((
+					s.id,
+					v.id,
+					i.id,
+					s.state,
+					s.progress,
+					s.start_time.strftime("%c")
+				)))
+
+	return
+
 @cli.group('volumes')
 def volumes():
 	"""Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+	help="Only instances for project (tag Project:<name>)")
+def list_volumes(project):
+	"List Voumes"
+	instances = filter_instances(project)
+
+	for i in instances:
+		for v in i.volumes.all():
+			print(", ".join((
+			v.id,
+			i.id,
+			v.state,
+			str(v.size) + "GiB",
+			v.encrypted and "Encrypted" or "Not Encrypted"
+			)))
+
+	return
 
 @cli.group('instances')
 def instances():
@@ -57,7 +102,11 @@ def stop_instances(project):
 
 	for i in instances:
 		print("Stopping {0}...".format(i.id))
-		i.stop()
+		try:
+			i.stop()
+		except botocore.exceptions.ClientError as e:
+			print(" Could not stop {0}.".format(i.id) + str(e))
+		continue
 
 	return
 
@@ -70,7 +119,34 @@ def start_instances(project):
 
 	for i in instances:
 		print("Starting {0}...".format(i.id))
+		try:
+			i.start()
+		except botocore.exceptions.ClientError as e:
+			print(" Could not start {0}.".format(i.id) + str(e))
+		continue
+
+	return
+
+@instances.command('snapshots')
+@click.option('--project', default=None,
+	help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+	"Create Snapshots of all volumes"
+	instances = filter_instances(project)
+
+	for i in instances:
+		print("Stopping instance of {0}".format(i.id))
+		i.stop()
+		i.wait_until_stopped()
+		for v in i.volumes.all():
+			print("Creating snapshot of {0}".format(v.id))
+			v.create_snapshot(Description="Created by SnapshotAnalyzer 30000")
+
+		print("Starting intance {0}".format(i.id))
 		i.start()
+		i.wait_until_running()
+
+	print("Jobs done")
 
 	return
 
