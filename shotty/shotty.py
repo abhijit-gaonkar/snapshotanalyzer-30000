484dbca34cc5fb623bc74b82238c075e31f903e4
Abhijit Gaonkar <gaonkara@dnb.com>
2019-11-27 16:28:51 -0800

    show all snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -27,7 +27,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
 	help="Only instances for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+	help="List all snapshots from each volume, not just the most recent one")
+def list_snapshots(project,list_all):
 	"List Snapshots"
 	instances = filter_instances(project)
 
@@ -42,6 +44,8 @@ def list_snapshots(project):
 					s.progress,
 					s.start_time.strftime("%c")
 				)))
+
+				if s.state == 'completed' and not list_all: break
 
 	return
 
